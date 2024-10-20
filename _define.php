6a4c9a26caba75fcef4291f85a59bcbd7237824b
Johan Cwiklinski <johan@x-tnd.be>
2024-10-20 09:45:09 +0200

    Bump version

--- _define.php
+++ _define.php
@@ -25,10 +25,10 @@ $this->register(
     'Galette Objects Lend',             //Name
     'Manage rent/lend of object',       //Short description
     'Mélissa Djebel, Johan Cwiklinski', //Author
-    '2.1.1',                            //Version
+    '2.1.2',                            //Version
     '1.1.0',                            //Galette version compatibility
     'objectslend',                      //routing name and translation domain
-    '2024-07-06',                       //Date
+    '2024-10-20',                       //Date
     [
         'objectslend_preferences'       => 'admin',
         'store_objectlend_preferences'  => 'admin',
